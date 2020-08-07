20eb93f76980593f6d6628b6e7ec88131dd2279d
RyanMatte <ryanmatte55@gmail.com>
2020-08-07 19:41:21 -0400

    Database has been added

---
+++ HackInstead/app/src/main/java/com/example/hackinstead/DatabaseAccess.java
@@ -0,0 +1,37 @@
+package com.example.hackinstead;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseAccess {
+    private SQLiteOpenHelper openHelper;
+    private SQLiteDatabase db;
+    private static DatabaseAccess instance;
+    Cursor c = null;
+
+    private DatabaseAccess(Context context) {
+        this.openHelper = new DatabaseOpenHelper(context);
+
+
+    }
+
+    public static DatabaseAccess getInstance(Context context) {
+        if (instance == null) {
+            instance = new DatabaseAccess(context);
+        }
+        return instance;
+    }
+
+    public void open() {
+        this.db = openHelper.getWritableDatabase();
+    }
+
+    public void close() {
+        if (db != null) {
+            this.db.close();
+        }
+    }
+}
---
+++ HackInstead/app/src/main/java/com/example/hackinstead/DatabaseOpenHelper.java
@@ -0,0 +1,12 @@
+package com.example.hackinstead;
+
+import android.content.Context;
+
+public class DatabaseOpenHelper extends com.readystatesoftware.sqliteasset.SQLiteAssetHelper {
+    public static final String DATABASE_NAME = "rolleroaster.db";
+    public static final int DATABASE_VERSION = 1;
+
+    public DatabaseOpenHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+}
