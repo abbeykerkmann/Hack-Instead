5e6609a5ebc53cc59b2ffe0b6223d2e66fc697e2
RyanMatte <ryanmatte55@gmail.com>
2020-08-08 19:56:07 -0400

    added save state

---
+++ HackInstead/app/src/main/java/com/example/hackinstead/Saving.java
@@ -0,0 +1,14 @@
+package com.example.hackinstead;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Saving extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_saving);
+    }
+}
