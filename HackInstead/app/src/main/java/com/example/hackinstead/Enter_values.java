b2e2497ee617a6fce32ac090e737ab116bbd5ed3
RyanMatte <ryanmatte55@gmail.com>
2020-08-08 10:11:11 -0400

    added a new activity

---
+++ HackInstead/app/src/main/java/com/example/hackinstead/Enter_values.java
@@ -0,0 +1,14 @@
+package com.example.hackinstead;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Enter_values extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter_values);
+    }
+}
